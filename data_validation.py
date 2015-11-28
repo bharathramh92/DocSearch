44ce4ae784cd6444b58d02d90d14eedc6fc6600a
bharath <bharathramh92@gmail.com>
2015-11-28 17:49:38 -0500

    basic data validation before inverted index

---
+++ data_validation.py
@@ -0,0 +1,16 @@
+import json
+
+def main():
+    data, ids = [], set()
+    with open("Resources/data_keywords", encoding="utf-8") as fl:
+        for line in fl.readlines():
+            data.append(line)
+            js = json.loads(line)
+            if js["id"] not in ids:
+                ids.add(js["id"])
+    if len(data) == len(ids):
+        print("All data have unique ids")
+    else:
+        print("All data doesn't have unique ids")
+if __name__ == '__main__':
+    main()
