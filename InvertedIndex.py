9e7b0d38450d7bdb657ed359daffb6ed60d14e4b
bharath <bharathramh92@gmail.com>
2015-11-28 23:41:50 -0500

    added category

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -74,18 +74,29 @@ def main():
                 isbn_combinations_dict[isbn.lower()] = [book["id"]]
         return tuple(isbn_combinations_dict.items())
 
+    def category_zone_map_helper(line):
+        entity = "categories"
+        lemmatizer = WordNetLemmatizer()
+        book = json.loads(line)
+        category_combinations_dict = {}
+        if entity in book:
+            for category in book[entity]:
+                for cat_split in re.findall(r"[a-zA-Z]+", category):
+                    category_combinations_dict[lemmatizer.lemmatize(cat_split.lower())] = [book["id"]]
+        return tuple(category_combinations_dict.items())
+
     author_zone = data.flatMap(author_zone_map_helper).reduceByKey(lambda x, y: x + y)
     title_zone = data.flatMap(title_zone_map_helper).reduceByKey(lambda x, y: x + y)
     publisher_zone = data.flatMap(publisher_zone_map_helper).reduceByKey(lambda x, y: x + y)
-    keyWords_zone = data.flatMap(keyWords_zone_map_helper).reduceByKey(lambda x, y: x + y)
+    key_words_zone = data.flatMap(keyWords_zone_map_helper).reduceByKey(lambda x, y: x + y)
     isbn_zone = data.flatMap(isbn_zone_map_helper).reduceByKey(lambda x, y: x + y)
-
+    category_zone = data.flatMap(category_zone_map_helper).reduceByKey(lambda x, y: x + y)
     author_zone.saveAsTextFile("Resources/author_rdd")
     title_zone.saveAsTextFile("Resources/title_rdd")
     publisher_zone.saveAsTextFile("Resources/publisher_rdd")
-    keyWords_zone.saveAsTextFile("Resources/keyWords_rdd")
+    key_words_zone.saveAsTextFile("Resources/keyWords_rdd")
     isbn_zone.saveAsTextFile("Resources/isbn_rdd")
-
+    category_zone.saveAsTextFile("Resources/category_rdd")
     sc.stop()
 
 if __name__ == '__main__':
