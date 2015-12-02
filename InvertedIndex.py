725b6ddfe6b1d4a3e8218d97110005bea4d731fc
bharath <bharathramh92@gmail.com>
2015-12-01 23:43:10 -0500

    comments were added

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -1,5 +1,4 @@
 #using spark
-import sys
 import os
 from pyspark import SparkContext
 import json
@@ -24,25 +23,25 @@ def main():
         entities = ["ISBN_10", "ISBN_13", "categories", "authors", "title", "publisher", "keyWords"]
         lemma_entities = ["title", "categories", "keyWords"]
         lemmatizer = WordNetLemmatizer()
-        book = json.loads(line)
+        book = json.loads(line)     # loads each book which were in a row of id_doc_rdd_raw
         index = []
         for entity in entities:
-            for book_id, book_data in book.items():
-                if entity in book_data:
+            for book_id, book_data in book.items():     # book data key is doc id and value is the book data
+                if entity in book_data:                 # value is a dictionary with entity/zone mapping to its value
                     value_for_entity = book_data[entity]
-                    if isinstance(value_for_entity, str):
-                        value_for_entity = [value_for_entity]
-                    for term in value_for_entity:
-                        for term_split in re.findall(r"[a-zA-Z0-9\-]+", term):
-                            term_split = term_split.lower()
+                    if isinstance(value_for_entity, str):   # if the entity value is string, convert it to list
+                        value_for_entity = [value_for_entity]   # eg: "isbn":"123" --> "isbn" = ["123",]
+                    for term in value_for_entity:               # each entity value is taken
+                        for term_split in re.findall(r"[a-zA-Z0-9\-]+", term):  # multiple words are split and indexed
+                            term_split = term_split.lower()     # eg: thomas h cormen --> into ['thomas', 'h', 'cormen']
                             if entity in lemma_entities:
-                                term_split = lemmatizer.lemmatize(term_split)
+                                term_split = lemmatizer.lemmatize(term_split)   # lemmatizing if they have to be
                             index.append((term_split, ((book_id, entity), )))
         return index
 
+    # flatmap is used since multiple key, value pairs are to be send out in map phase
     index_rdd = data.flatMap(index_map_helper).reduceByKey(lambda x, y: x + y)
     index_rdd.saveAsTextFile("Resources/index_rdd")
-
     sc.stop()
 
 if __name__ == '__main__':
