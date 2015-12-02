5c487ad94a95bffafb1daf2c7371bb7cd12d0d4d
bharath <bharathramh92@gmail.com>
2015-12-01 23:03:44 -0500

    fixed a crucial part of defining term_documents in an outer scope. previuosly compound addition of doc ids were there. improved and efficiency

--- Query.py
+++ Query.py
@@ -15,34 +15,42 @@ model_weightage = {'ISBN_10': .3, 'ISBN_13': .3, 'authors': .18, 'publisher': .1
 zone_list = ['publisher', 'authors', 'categories', 'keyWords', 'ISBN_13', 'title', 'ISBN_10']
 
 
-def get_docs(query_term, zone_restriction=None):
+def get_docs(query_term=None, zone_restriction=None):
     """
 
-    :param query_term:
-    :param zone_restriction: list of zone name where the query term has to be matched with
+    :param query_term: search term if it has to be searched across all the zones.
+    :param zone_restriction: dictionary whose keys are zone and the corresponding value is the search term of that zone.
     :return:
     """
-    zone_rdd = sc.textFile("Resources/index_rdd")
-    query_term_docs, lemmatizer = {}, WordNetLemmatizer()
-    term_ids_mapping = {}
-    term_combos = set()
+    if query_term is None and zone_restriction is None:
+        # Validation to make sure that search term is provided
+        print("Either query_term of zone_restriction should passed.")
+        exit(1)
+
+    zone_rdd = sc.textFile("Resources/index_rdd")  # input rdd
+    query_term_docs = {}   # in order to get weighted score, terms are mapped to a dictionary of doc id to zone mapping.
+    lemmatizer = WordNetLemmatizer()
+    term_ids_mapping = {}   # to map term with positive search result document ids
+    term_combos = set()     # to store possible combinations of the search term
     if zone_restriction is None:
+        # normalizing the search term to dictionary format of zone_restriction
         zone_restriction = {}
         for zn in zone_list:
             zone_restriction[zn] = query_term
-    zone_restriction_added_terms = defaultdict(list)
+    zone_restriction_added_terms = defaultdict(list)    # expand the zone_restriction to subset of the individual terms.
     for zone_res, q_term in zone_restriction.items():
         for term in re.findall(r"([\w]+[\-]*[\w]+)", q_term):
-            # Getting each term from the given search term
+            # Getting individual words from the given search term
             if len(term.split("-")) > 1:
                 if len(re.findall(r"[a-zA-Z]+[\-]+[\w]+", term)) > 0:
                     # if the term is a character and has a hiphen, split the term at the hiphen position
+                    # 'abcd-efgh' will be transformed into ['abcd', 'efgh']
                     for tm in term.split("-"):
                         term_combos.add(tm)
                         zone_restriction_added_terms[zone_res].append(tm)
                 if len(re.findall(r"[0-9]+[\-]+[\w]+", term)) > 0:
-                    # if the term is a number and has a hiphen, concatenate the term
-                    # useful in isbn search were the isbn could be given like ‎978-3-16-148410-0
+                    # if the term is a number and has a hiphen, concatenate the term. Mainly for isbn check
+                    # eg: ‎978-3-16-148410-0 is transformed into 9783161484100
                     combined_term = ''.join(term.split("-"))
                     term_combos.add(combined_term)
                     zone_restriction_added_terms[zone_res].append(combined_term)
@@ -55,15 +63,14 @@ def get_docs(query_term, zone_restriction=None):
         lemma_term = lemmatizer.lemmatize(term)
         term_combos.add(lemma_term)
 
-    term_documents = defaultdict(list)
-
     print(term_combos)
-    def raw_map_helper(line):
-        line = eval(line)
-        return line[0] in term_combos
 
     # Performing data retrieval/filter using Spark
     # raw_docs_collections = zone_rdd.filter(lambda line: eval(line)[0] in term_combos)
+    def raw_map_helper(line):
+        line = eval(line)
+        # line[0] corresponds to index term
+        return line[0] in term_combos
     raw_docs_collections = zone_rdd.filter(raw_map_helper)
     docs_collect = raw_docs_collections.collect()   # list of documents from search query
 
@@ -72,46 +79,49 @@ def get_docs(query_term, zone_restriction=None):
         for term_doc_zone in docs_collect:
             term_doc_zone = eval(term_doc_zone)
             term = term_doc_zone[0]
+            term_documents = defaultdict(list)  # mapping doc id with zones
             # data format: term_doc_zone: (index_term, tuple((document_id, zone_name), ))
             doc_zone = term_doc_zone[1]
-            ids = set()     # to store the docs which matches to the zone condition
+            ids = set()     # to store the docs for corresponding term
             for doc, zone in doc_zone:
                 if term in zone_restriction_added_terms[zone]:
+                    # save the result only if the data corresponds to the given zone
                     ids.add(doc)
                     term_documents[doc].append(zone)
             term_ids_mapping[term] = ids
             query_term_docs[term] = term_documents
 
-    # And operation for all the term result
+    # And operations for the term results
+    # sort the query terms based on the result size
     terms_names_sorted = sorted(term_ids_mapping, key=lambda k: len(term_ids_mapping[k]))
     print("Records for each search terms")
-    for k in terms_names_sorted:
-        print(k, " ", len(term_ids_mapping[k]))
-    anded_result = term_ids_mapping[terms_names_sorted[0]]
-    # anded_result = set()
+    for q_term_name in terms_names_sorted:
+        print(q_term_name, " ", len(term_ids_mapping[q_term_name]))
+    anded_result = term_ids_mapping[terms_names_sorted[0]]  # storing the small sized result to anded_result
     if len(terms_names_sorted) > 1:
         for term in terms_names_sorted[1:]:
-            # print(term, " , ", anded_result)
             if len(anded_result) <= len(term_ids_mapping[term]):
+                # term_ids_mapping[term] outputs a set of doc ids for "term"
                 short = anded_result
                 long = term_ids_mapping[term]
             else:
                 long = anded_result
                 short = term_ids_mapping[term]
+            # pre sorting short and long lists makes the search faster
             short = sorted(short)
             long = sorted(long)
+            long_index = 0
             for item_in_short in short:
-                if item_in_short in long:
-                    anded_result.add(item_in_short)
-            # previous_computed_doc_ids = anded_result
-    # else:
-    #     anded_result.update(term_ids_mapping[previous_computed_doc_ids])
+                for l_i in range(0, len(long)):
+                    if item_in_short == long[l_i]:
+                        anded_result.add(item_in_short)
+                    long_index += 1
 
     return query_term_docs, anded_result
 
 
 def main():
-    q_term = 'algorithm'
+    q_term = 'algorithm big'
     # q_term = "9781478427674"
     # q_term = "978-1478427674"
 
